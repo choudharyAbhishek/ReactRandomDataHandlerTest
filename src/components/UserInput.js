09e3af7520963bf75bd100ea1aa68811db2a38ae
Abhishek Choudhary <abhishekchoudhary@abhisheks-mbp.hq.k.grp>
2018-12-03 11:18:51 +0000

    created base design

---
+++ src/components/UserInput.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import '../styles/ComponentStyles.css'
+
+class UserInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numberOfRows: 0,
+            numberOfColumns: '',
+            spaceBetweenRows: 0,
+            spaceBetweenBoxes: 0,
+            errorText: '',
+            errorValue: false
+        }
+        this.updateRowsCount = this.updateRowsCount.bind(this);
+        this.updateColumnCount = this.updateColumnCount.bind(this);
+        this.updateRowsGap = this.updateRowsGap.bind(this);
+        this.updateBoxesGap = this.updateBoxesGap.bind(this);
+        this.updateTableView = this.updateTableView.bind(this);
+    }
+
+    updateTableView() {
+        let totalRows = this.state.numberOfRows;
+        let totalColumn = this.state.numberOfColumns;
+        //let spaceBetweenRow = this.state.spaceBetweenRows;
+        //let spaceBetweenBox = this.state.spaceBetweenBoxes;
+        console.log(totalRows);
+        //const table = document.createElement("table")
+/*         for(let i=0; i<totalRows; i++){
+            var newRow = table.insertRow();
+            for(let j=0; j<totalColumn; j++){
+                //var column = document.createElement("td");
+                newRow.insertCell();
+                console.log("here");
+            }
+            //table.appendChild(row);
+        }
+                var rows = [];
+        this.state.numberOfRows.forEach(function(numberOfRows) {
+            rows.push(
+              <tr />
+                <td><SomePopulationComponent /></td>
+                <td><SomeZoneComponent /></td>
+              </tr>
+            );
+        }.bind(this));
+
+        */
+
+        //return table;
+    }
+
+    updateRowsCount(e) {
+        if(e.target.value > 0)
+            this.setState({
+                numberOfRows : e.target.value
+            });
+        else {
+            this.setState({
+                errorText : 'please add a larger number for rows',
+                errorValue : true
+            });
+        }
+        this.updateTableView();
+    }
+
+    updateColumnCount(e) {
+        this.setState({
+            numberOfColumns : e.target.value
+        });
+        this.getColumnSpecifications();
+        this.updateTableView();
+    }
+
+    updateRowsGap(e) {
+        if(e.target.value >= 0)
+            this.setState({
+                spaceBetweenRows : e.target.value
+            });
+        else {
+            this.setState({
+                errorText : 'please add a larger number for rows gap',
+                errorValue : true
+            });
+        }
+    }
+
+    updateBoxesGap(e) {
+        if(e.target.value >= 0)
+            this.setState({
+                spaceBetweenBoxes : e.target.value
+            });
+        else {
+            this.setState({
+                errorText : 'please add a larger number for boxes gap',
+                errorValue : true
+            });
+        }
+    }
+
+    getColumnSpecifications() {
+        let columnText = this.state.numberOfColumns;
+        let columnSplits = columnText.split(',');
+        return(columnSplits)
+    }
+
+    render() {
+        return (
+            <div>
+            {this.state.errorValue && <h2>{this.state.errorText}</h2>}
+                <div id= "userInputsContainer">
+                    <input type="number" id= "userInputs" placeholder="No. of rows" onChange={this.updateRowsCount}/>
+                    <input type="text" id= "userInputs" placeholder="column string" onChange={this.updateColumnCount}/>
+                    <input type="number" id= "userInputs" placeholder="space between rows" onChange={this.updateRowsGap}/>
+                    <input type="number" id= "userInputs" placeholder="space between boxes" onChange={this.updateBoxesGap}/>
+                </div>
+                <div>
+                    {this.updateTableView()}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default UserInput;
